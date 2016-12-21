e5d8d679140ec47ef51c75d16b33a21f5b36c9b2
Yael Vaisbuch <yael.vaisbuch@gmail.com>
2016-12-21 12:48:07 -0300

    V20 original

---
+++ newsletter.php
@@ -0,0 +1,48 @@
+<?php
+
+// get posted data into local variables
+//$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
+$EmailTo = "contacto@tclchile.cl";
+$Subject = "Inscripción a Newsletter desde web TCL";
+$email ="Newsletter";
+
+// validation
+$validationOK=true;
+// if (Trim($EmailFrom)=="") $validationOK=false;
+// if (!$validationOK) {
+//   echo '<script language="javascript">alert("Lo sentimos, porfavor ingrese un email valido");</script>';
+//   exit;
+// }
+
+// campos nombres pymes
+$EmailFrom = Trim(stripslashes($_POST['desdeMail'])); 
+
+// prepare email body text
+$Body = "";
+$Body .= "Mail de inscripción: ";
+$Body .= $EmailFrom;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From:". $email);
+
+// redirect to success page 
+if ($success){
+    ?>
+    <script language = 'javascript'>
+    alert('Gracias, hemos recibido satisfactoriamente su email.');
+	history.go(-1);
+	</script>
+	<?
+//  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.htm\">";
+}
+else{
+?>
+<script language = 'javascript'>
+    alert('Algo a salido mal, porfavor intentelo nuevamente.');
+    history.go(-1);
+</script>
+<?
+//  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
